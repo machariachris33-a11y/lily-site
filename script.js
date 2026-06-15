beb9e44dde4392b94d1edb0899a944da4332096c
machariachris33-a11y <machariachris33@gmail.com>
2026-06-15 11:27:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const text = 
+"Hey my love 🌸\n\n" +
+"I just wanted to remind you of something before your day begins...\n\n" +
+"You are beautiful.\n" +
+"You are strong.\n" +
+"You are deeply loved.\n\n" +
+"No matter how heavy today feels, you are not alone.\n" +
+"Take your time. Breathe. You are doing enough.";
+
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typewriter").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 40);
+  }
+}
+
+window.onload = typeWriter;
+
 function showMessage() {
   document.getElementById("msg").style.display = "block";
 }
